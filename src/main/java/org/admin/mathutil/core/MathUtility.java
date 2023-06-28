b59d9bea3128b6c18c8aa343f43152b3c1f44d6d
nguyyenminhvu <nguyyenminhvu@gmail.com>
2023-06-28 11:29:20 +0700

    Update

--- src/main/java/org/admin/mathutil/core/MathUtility.java
+++ src/main/java/org/admin/mathutil/core/MathUtility.java
@@ -5,12 +5,24 @@ public class MathUtility {
 
     public static final double PI = 3.1415;
 
+//    public static long getFatorial(int n) {
+//        if (n < 0) {
+//            throw new IllegalArgumentException("Factorial is not defined for negative numbers.");
+//        } else if (n > 20) {
+//            throw new IllegalArgumentException("Factorial greater than 20 is too big.");
+//        } else if (n == 0 || n == 1) {
+//            return 1;
+//        } else {
+//            return n * getFatorial(n - 1);
+//        }
+//    }
     public static long getFatorial(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Factorial is not defined for negative numbers.");
         } else if (n > 20) {
             throw new IllegalArgumentException("Factorial greater than 20 is too big.");
-        } else if (n == 0 || n == 1) {
+        }
+        if (n == 0 || n == 1) {
             return 1;
         } else {
             return n * getFatorial(n - 1);
@@ -43,33 +55,28 @@ public class MathUtility {
             return x;
         }
     }
-    
-    public static long getPow(double n,double pow) {
-        if(pow == 0 || n == 1)
-        {
+
+    public static long getPow(double n, double pow) {
+        if (pow == 0 || n == 1) {
             return 1;
-        }else if(n == 0)
-        {
+        } else if (n == 0) {
             return 0;
-        }
-        else if(pow == 1)
-        {
-            return (long)n;
-        }else
-        {
-        double result = 1;
-        boolean isNegativeExponent = pow < 0;
-        pow = getAbsolute((long)pow);
-        
-        for (int i = 0; i < pow; i++) {
-            result *= n;
-        }
-        
-        if (isNegativeExponent) {
-            result = 1 / result;
-        }
-        
-        return (long) result;
+        } else if (pow == 1) {
+            return (long) n;
+        } else {
+            double result = 1;
+            boolean isNegativeExponent = pow < 0;
+            pow = getAbsolute((long) pow);
+
+            for (int i = 0; i < pow; i++) {
+                result *= n;
+            }
+
+            if (isNegativeExponent) {
+                result = 1 / result;
+            }
+
+            return (long) result;
         }
     }
 }
